f0f032667adc1ec7b66187d056452801da533201
moviefan322 <philman202@yahoo.com>
2023-07-16 11:43:38 -0400

    connects to postgres DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { FlashcardsModule } from './flashcards/flashcards.module';
 import { StatsModule } from './stats/stats.module';
 import { LessonModule } from './lessons/lessons.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [FlashcardsModule, StatsModule, LessonModule],
+  imports: [
+    FlashcardsModule,
+    StatsModule,
+    LessonModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5434,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'spanish-app',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
