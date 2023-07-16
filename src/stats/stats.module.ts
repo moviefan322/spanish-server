78d2a6530453529509394b3341668083d6f6d5ee
moviefan322 <philman202@yahoo.com>
2023-07-16 11:28:16 -0400

    updates imports/exports for stat module

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StatsController } from './stats.controller';
 import { StatsService } from './stats.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Stat } from './stat.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Stat])],
   controllers: [StatsController],
-  providers: [StatsService]
+  providers: [StatsService],
+  exports: [StatsService],
 })
 export class StatsModule {}
