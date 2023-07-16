fd7cf9f6b077efc7670dd1c089f52f7bc7f5bc43
moviefan322 <philman202@yahoo.com>
2023-07-16 16:02:33 -0400

    hashes password, makes email unique and verifies email is email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,13 +18,25 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
     const { username, password, email } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.repo.create({
       username,
-      password,
+      password: hashedPassword,
       email,
     });
-    await this.repo.save(user);
-    console.log('user', user);
-    return user;
+    try {
+      await this.repo.save(user);
+      return user;
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,11 +1,13 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail } from 'class-validator';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ unique: true })
+  @IsEmail()
   email: string;
 
   @Column()
