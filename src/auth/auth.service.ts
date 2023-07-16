9d38727fd69d80cfe3494072494a9ffb6d2573a8
moviefan322 <philman202@yahoo.com>
2023-07-16 15:51:13 -0400

    adds signup route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private repo: Repository<User>,
+  ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password, email } = authCredentialsDto;
+    const user = this.repo.create({
+      username,
+      password,
+      email,
+    });
+    await this.repo.save(user);
+    console.log('user', user);
+  }
+}
