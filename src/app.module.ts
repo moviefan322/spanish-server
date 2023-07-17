be0f70e3bccceeebcd79a17cb6ba35c319f28fcd
moviefan322 <philman202@yahoo.com>
2023-07-17 09:38:37 -0400

    adds validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { LessonModule } from './lessons/lessons.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: [`.env.stage.${process.env.STAGE}`] }),
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
