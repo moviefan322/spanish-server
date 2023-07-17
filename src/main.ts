6c0251316026187d36499c4775b2f382f3face99
moviefan322 <philman202@yahoo.com>
2023-07-17 10:05:02 -0400

    applies CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3001);
